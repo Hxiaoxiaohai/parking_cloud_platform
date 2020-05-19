2c8ada2a7e79ff3b04673132d19ec61f1189ecfd
zhongchao <zhongchao6725@163.com>
2020-05-19 17:38:30 +0800

    菜单HTML

--- js/cloud_platform/index/menu.js
+++ js/cloud_platform/index/menu.js
@@ -3,9 +3,7 @@ $(function () {
         function (data) {
             //console.log(data);
 
-            // var menus = '';
             // 菜单HTML打印
-
             if (data.code == 0) {
                 var zNodes = JSON.parse(JSON.stringify(data.data));
                 //console.log(zNodes)
@@ -25,40 +23,12 @@ $(function () {
  * @Dec: 菜单HTML生成
  */
 
-// $(function () {
-//     var data = [
-//         { id: 1, name: "办公管理", pid: 0 },
-//         { id: 2, name: "请假申请", pid: 1 },
-//         { id: 3, name: "出差申请", pid: 1 },
-//         { id: 4, name: "请假记录", pid: 2 },
-//         { id: 5, name: "系统设置", pid: 0 },
-//         { id: 6, name: "权限管理", pid: 5 },
-//         { id: 7, name: "用户角色", pid: 6 },
-//         { id: 8, name: "菜单设置", pid: 6 },
-//     ];
-//     getMenuHTML(0, data);
-//     $("#app").append(menus);
-// });
-
-
 //菜单列表html
 var menus = '';
 
 //根据菜单主键id生成菜单列表html
 //id：菜单主键id
 //arry：菜单数组信息
-// function getMenuHTML(id, arry) {
-//     var childArry = GetParentArry(id, arry);
-//     if (childArry.length > 0) {
-//         menus += '<ul>';
-//         for (var i in childArry) {
-//             menus += '<li>' + childArry[i].name;
-//             getMenuHTML(childArry[i].id, arry);
-//             menus += '</li>';
-//         }
-//         menus += '</ul>';
-//     }
-// }
 function getMenuHTML(id, arry) {
     var childArry = GetParentArry(id, arry);
     if (childArry.length > 0) {
